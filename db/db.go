605d23550be302b9d8469533e7b40150e65b892c
nntakuya <uv.nkmt.yii@gmail.com>
2020-01-25 16:49:16 +0900

    first commit

---
+++ db/db.go
@@ -0,0 +1,38 @@
+package db
+
+import (
+  "github.com/jinzhu/gorm"
+  _ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+  db *gorm.DB
+  err error
+)
+
+// Init is initialize db from mainfunction
+func Init() {
+  db, err = gorm.Open("mysql", "root:password@(:3030)/todo_dev")
+
+  if err != nil {
+    panic(err)
+  }
+}
+
+// GetDB is called in models
+func GetDB() *gorm.DB {
+  return db
+}
+
+
+// Close is closing db
+func Close() {
+  if err := db.Close(); err != nil {
+    panic(err)
+  }
+}
+
+
+
+
+
