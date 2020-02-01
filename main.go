0fe642836a2acc51dabb4e69f029b16387c24d5e
nntakuya <uv.nkmt.yii@gmail.com>
2020-02-02 00:46:55 +0900

    main.goを変更

--- main.go
+++ main.go
@@ -1,21 +1,14 @@
 package main
 
 import (
-  "github.com/gin-gonic/gin"
   "github.com/nntakuya/todo_app/db"
+  "github.com/nntakuya/todo_app/server"
 )
 
 func main() {
-  r := gin.Default()
-  r.GET("/", func(c *gin.Context) {
-    c.String(200, "Hello, World")
-  })
-
   db.Init()
 
-  r.Run()
-
-  db.Close()
+  server.Init()
 }
 
 
